0ccc63053b8601f512357af6de1a45b706f766b8
Zaigham Salehi <zsalehi313@gmail.com>
2022-08-10 22:19:23 -0400

    Added table to library

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let myLibrary = [];
 
-function Book () {
-
+function Book (title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
 }
 
 function addBookToLibrary () {
+    const title = prompt("What is the name of the book?");
+    const author =  prompt("What is the author of the book?");
+    const book = new Book(title, author);
+    myLibrary.push(book);
+}
+
+myLibrary.forEach(displayBook(book))
+
+function createNewRow () {
+    //find the table
+    const table = document.getElementById("library");
+    const row = table.insertRow(); 
+
+}
+
+function displayBook(book) {
     
 }
